2f83db8f851b9c5142df6ad2b25614b25899694f
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-06 17:51:03 +0300

    Добавление в MainScreen.kt навигации

--- app/src/main/java/com/example/tinn/ui/features/main/MainScreen.kt
+++ app/src/main/java/com/example/tinn/ui/features/main/MainScreen.kt
@@ -1,10 +1,19 @@
-package com.example.tinn.ui.features
+package com.example.tinn.ui.features.main
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.tinn.ui.navigation.MainBottomNavigation
+import com.example.tinn.ui.navigation.MainNavHost
 
 @Composable
 fun MainScreen() {
-    Text(text = "Main", style = MaterialTheme.typography.h3)
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { MainBottomNavigation(navController = navController)}
+    ) { paddingValues ->
+        MainNavHost(mainNavController = navController, modifier = Modifier.padding(paddingValues))
+    }
 }
