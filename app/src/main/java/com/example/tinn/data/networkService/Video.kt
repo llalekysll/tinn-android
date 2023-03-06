61cc6d36a4a32e3172f5fffdaf54c1ef91321eaf
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-07 00:49:05 +0300

    Создание интерфейса Video.kt

---
+++ app/src/main/java/com/example/tinn/data/networkService/Video.kt
@@ -0,0 +1,28 @@
+package com.example.tinn.data.networkService
+
+import com.example.tinn.data.emptities.Comment
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface Video {
+    @GET("video/categories")
+    fun getCategories(@Header("Authorization") token: String): Call<Any>
+
+    @GET("video/comments")
+    fun getComments(
+        @Query("video_id") videoId: String,
+        @Query("page") page: Int,
+        @Header("Authorization") token: String
+    ): Call<Comment>
+
+    @GET("video")
+    fun getVideo(@Header("Authorization") token: String): Call<List<Video>>
+
+    @GET("video/recommended")
+    fun getRecommendedVideo(@Header("Authorization") token: String): Call<List<Video>>
+
+    @GET("video/popular")
+    fun getPopularVideo(@Header("Authorization") token: String): Call<List<Video>>
+}
