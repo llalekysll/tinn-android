5ce11ce9ffce96899eb7da8d7450ce414eb1fc92
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-06 17:49:26 +0300

    Добавление label

--- app/src/main/java/com/example/tinn/ui/navigation/Screens.kt
+++ app/src/main/java/com/example/tinn/ui/navigation/Screens.kt
@@ -9,11 +9,11 @@ sealed class Screens(val route: String) {
     object Main : Screens("main")
 }
 
-sealed class MainScreens(val route: String, val idIcon: Int) {
-    object Home : MainScreens("home", R.drawable.ic_home)
-    object Popular : MainScreens("popular", R.drawable.ic_podium)
-    object Forum : MainScreens("forum", R.drawable.ic_forum)
-    object Application : MainScreens("application", R.drawable.ic_money)
-    object Room : MainScreens("room", R.drawable.ic_room)
-    object Marketplace : MainScreens("marketplace", R.drawable.ic_phone)
+sealed class MainScreens(val route: String, val label: String, val idIcon: Int) {
+    object Home : MainScreens("home", "Главная", R.drawable.ic_home)
+    object Popular : MainScreens("popular", "Популярное", R.drawable.ic_podium)
+    object Forum : MainScreens("forum", "Форум", R.drawable.ic_forum)
+    object Application : MainScreens("application", "Мои заявки", R.drawable.ic_money)
+    object Room : MainScreens("room", "My room", R.drawable.ic_room)
+    object Marketplace : MainScreens("marketplace", "Marketplace", R.drawable.ic_phone)
 }
