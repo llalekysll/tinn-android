7d9fce87b8fed291be82e0762a0afe1784ff8b44
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-06 16:56:26 +0300

    Написание ретрофит клиента

---
+++ app/src/main/java/com/example/tinn/data/networkService/RetrofitClient.kt
@@ -0,0 +1,19 @@
+package com.example.tinntest.data.networkService
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private var retrofitService: Retrofit? = null
+
+    fun getRetrofitService(): Retrofit {
+        if (retrofitService == null) {
+            retrofitService = Retrofit.Builder()
+                .baseUrl("https://auth.tinn.io/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        return  retrofitService!!
+    }
+}
