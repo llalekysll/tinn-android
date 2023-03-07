22f52d0fc9c21800437a517f6158cdf298465549
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-07 19:34:45 +0300

    создание ProfileModel.kt

---
+++ app/src/main/java/com/example/tinn/data/modelForJSON/ProfileModel.kt
@@ -0,0 +1,18 @@
+package com.example.tinn.data.modelForJSON
+
+import com.google.gson.annotations.SerializedName
+import org.intellij.lang.annotations.Language
+import java.util.Date
+
+data class ProfileModel(
+    val login: String,
+    @SerializedName("first_name")
+    val firstName: String,
+    @SerializedName("second_name")
+    val secondName: String,
+    val phone: String,
+    @SerializedName("gender_id")
+    val genderId: Int,
+    @SerializedName("data_of_birth")
+    val dataOfBirth: Date,
+)
