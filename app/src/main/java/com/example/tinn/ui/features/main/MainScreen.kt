01a19ec1c460bf63275da6e03b63fa4c031928b5
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-08 02:22:03 +0300

    Добавление Toolbar в Appbar

--- app/src/main/java/com/example/tinn/ui/features/main/MainScreen.kt
+++ app/src/main/java/com/example/tinn/ui/features/main/MainScreen.kt
@@ -1,18 +1,21 @@
 package com.example.tinn.ui.features.main
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.example.tinn.ui.components.Toolbar
 import com.example.tinn.ui.navigation.MainBottomNavigation
 import com.example.tinn.ui.navigation.MainNavHost
 
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
+
     Scaffold(
-        bottomBar = { MainBottomNavigation(navController = navController)}
+        topBar = { Toolbar() },
+        bottomBar = { MainBottomNavigation(navController = navController) }
     ) { paddingValues ->
         MainNavHost(mainNavController = navController, modifier = Modifier.padding(paddingValues))
     }
