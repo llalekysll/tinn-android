df052236cf3fbaac9f15bda0d5dd8c11fcaa4129
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-08 00:05:15 +0300

    Изменение констант

--- app/src/main/java/com/example/tinn/utils/Constants.kt
+++ app/src/main/java/com/example/tinn/utils/Constants.kt
@@ -2,4 +2,7 @@ package com.example.tinn.utils
 
 const val AUTHORIZATION = "authorization"
 const val TOKEN = "token"
-const val EMAIL_IS_CONFIRMATION = "emailIsConfirmed"
+const val STATUS_AUTHORIZATION = "status_authorization"
+const val VERIFICATION_EMAIL = "verification_email"
+const val INPUT_INFO_USER = "input_info_user"
+const val IS_AUTHORIZATION = "is_authorization"
