e1801fe83ba9f549f8a92d73e23f2021ce0d63a6
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-08 01:06:58 +0300

    Добавление DropMenu для аватара

--- app/src/main/java/com/example/tinn/ui/components/Toolbar.kt
+++ app/src/main/java/com/example/tinn/ui/components/Toolbar.kt
@@ -2,40 +2,56 @@ package com.example.tinn.ui.components
 
 import android.widget.Toolbar
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.IconButton
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.example.tinn.R
+import com.example.tinn.ui.theme.Red
 
 @Composable
 fun Toolbar() {
     TopAppBar(elevation = 20.dp, backgroundColor = Color.White) {
-        Row(
-            Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.logo),
-                contentDescription = "logo",
-                modifier = Modifier.size(100.dp)
-            )
-
-            IconButton(onClick = { }) {
+        var isExpand by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.End) {
+            Row(
+                Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 Image(
-                    painter = painterResource(id = R.drawable.ic_avatar),
-                    contentDescription = "profile",
-                    modifier = Modifier.size(40.dp)
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "logo",
+                    modifier = Modifier.size(100.dp)
                 )
+
+                IconButton(onClick = { isExpand = true }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_avatar),
+                        contentDescription = "profile",
+                        modifier = Modifier.size(40.dp)
+                    )
+                }
+            }
+
+            DropdownMenu(
+                expanded = isExpand,
+                onDismissRequest = { isExpand = false },
+                offset = DpOffset((-100).dp, 0.dp)
+            ) {
+                listOf("Мой канал", "Настройки", "Выход").forEach {
+                    DropdownMenuItem(onClick = { isExpand = false }) {
+                        Text(
+                            text = it,
+                            color = if (it == "Выход") Red else Color.Black
+                        )
+                    }
+                }
             }
         }
     }
