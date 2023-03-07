77fda9757e1c2e8945b9374433fc560653125610
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-08 02:14:08 +0300

    Пометка Headers no-authorization login и register и удаление Header authorization

--- app/src/main/java/com/example/tinn/data/networkService/AuthorizationService.kt
+++ app/src/main/java/com/example/tinn/data/networkService/AuthorizationService.kt
@@ -4,18 +4,20 @@ import com.example.tinn.data.modelForJSON.*
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface AuthorizationService {
+    @Headers("No-Authentication: true")
     @POST("register")
     fun register(@Body registerModel: RegisterModel): Call<ResponceAuthorizatinoModel>
 
+    @Headers("No-Authentication: true")
     @POST("login")
     fun login(@Body signInModel: SignInModel): Call<ResponceAuthorizatinoModel>
 
     @POST("verification/email")
     fun verificationEmail(
         @Body verificationModel: VerificationModel,
-        @Header("Authorization") token: String
     ): Call<ResponceVerificationModel>
 }
