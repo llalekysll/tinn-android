e14bfd7707938118ca0884232d4165e4f75bae36
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-08 23:42:55 +0300

    Реализация профиля

--- app/src/main/java/com/example/tinn/ui/features/main/profile/Profile.kt
+++ app/src/main/java/com/example/tinn/ui/features/main/profile/Profile.kt
@@ -1,20 +1,29 @@
 package com.example.tinn.ui.features.main.profile
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.tinn.ui.theme.AlphaBlack
 import com.example.tinn.ui.theme.Gray
 import com.example.tinn.viewModel.UserViewModel
 import com.skydoves.landscapist.glide.GlideImage
+import com.example.tinn.R
 
 @Composable
 fun ProfileScreen(navController: NavController) {
@@ -26,15 +35,35 @@ fun ProfileScreen(navController: NavController) {
     })
 
     user?.let {
-        Box {
+        Box(contentAlignment = Alignment.TopEnd) {
             GlideImage(
-                modifier = Modifier.fillMaxWidth().height(200.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp),
                 imageModel = "https://s0.rbk.ru/v6_top_pics/media/img/9/16/756619467602169.jpg")
+
+            IconButton(
+                onClick = {  },
+                modifier = Modifier
+                    .padding(top = 120.dp, end = 16.dp)
+                    .background(color = AlphaBlack, shape = RoundedCornerShape(8.dp))
+
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.plus),
+                    contentDescription = "Изменить фон",
+                    modifier = Modifier.size(12.dp),
+                    tint = Color.White
+                )
+            }
 
             Card(
                 elevation = 16.dp,
                 shape = RoundedCornerShape(topEnd = 16.dp, topStart = 16.dp),
-                modifier = Modifier.fillMaxWidth().fillMaxHeight().padding(top = 180.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(top = 180.dp),
             ) {
 
             }
