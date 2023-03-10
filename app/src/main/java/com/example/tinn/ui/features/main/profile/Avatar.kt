59c39f917b5badb3313260cbadde8105cd7c3b02
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-10 23:53:04 +0300

    Загрузка изображений

--- app/src/main/java/com/example/tinn/ui/features/main/profile/Avatar.kt
+++ app/src/main/java/com/example/tinn/ui/features/main/profile/Avatar.kt
@@ -1,19 +1,22 @@
 package com.example.tinn.ui.features.main.profile
 
-import androidx.compose.foundation.Canvas
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.drawText
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tinn.R
@@ -24,6 +27,24 @@ import com.skydoves.landscapist.glide.GlideImage
 @Composable
 fun Avatar(avatar: String?) {
     val viewModel: UserViewModel = viewModel()
+
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+
+    imageUri?.let {
+        viewModel.loadAvatar(it)
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+    contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageUri = uri
+    }
+
+    val statusRequest by viewModel.requestStatus.observeAsState()
+    if (statusRequest == "OK") {
+
+    }
+    if (statusRequest == "LOAD") CircularProgressIndicator()
 
     if (avatar != null) {
         GlideImage(
@@ -40,7 +61,9 @@ fun Avatar(avatar: String?) {
             modifier = Modifier
                 .padding(16.dp)
                 .size(80.dp)
-                .clickable {  }
+                .clickable {
+                    launcher.launch("image/*")
+                }
         )
     }
 }
