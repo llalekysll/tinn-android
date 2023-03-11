dc84458d9eeb08c9d006631e9a12700a61e04d87
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-11 11:29:53 +0300

    Добавление функций расширения получения дня недели

--- app/src/main/java/com/example/tinn/utils/ExtensionFunctions.kt
+++ app/src/main/java/com/example/tinn/utils/ExtensionFunctions.kt
@@ -8,3 +8,5 @@ fun String.emailIfValid(): Boolean {
 
 fun Calendar.getDate() = this.get(Calendar.DATE)
 fun Calendar.getMonth() = this.get(Calendar.MONTH)
+
+fun Calendar.getDayOfWeek() = this.get(Calendar.DAY_OF_WEEK)
