9a4bf88f38a24012fc0900d3fe3b82f21e7ac57a
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-11 11:29:01 +0300

    Добавление функций расширения для календаря

--- app/src/main/java/com/example/tinn/utils/ExtensionFunctions.kt
+++ app/src/main/java/com/example/tinn/utils/ExtensionFunctions.kt
@@ -1,5 +1,10 @@
 package com.example.tinn.utils
 
+import java.util.*
+
 fun String.emailIfValid(): Boolean {
     return (this.isEmpty() || this.matches(Regex("\\S*@\\S*[.]\\S*")))
 }
+
+fun Calendar.getDate() = this.get(Calendar.DATE)
+fun Calendar.getMonth() = this.get(Calendar.MONTH)
