edc47e032078cc5035f4a8088646dc2ab1bd0694
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-11 12:21:58 +0300

    Добавление функций getYar и parseDateToShortString

--- app/src/main/java/com/example/tinn/utils/ExtensionFunctions.kt
+++ app/src/main/java/com/example/tinn/utils/ExtensionFunctions.kt
@@ -1,5 +1,6 @@
 package com.example.tinn.utils
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 fun String.emailIfValid(): Boolean {
@@ -8,5 +9,11 @@ fun String.emailIfValid(): Boolean {
 
 fun Calendar.getDate() = this.get(Calendar.DATE)
 fun Calendar.getMonth() = this.get(Calendar.MONTH)
+fun Calendar.getYear() = this.get(Calendar.YEAR)
+
+fun Calendar.parseDateToShortString(): String? {
+    val format = SimpleDateFormat("EE. dd MMM")
+    return format.format(this.time)
+}
 
 fun Calendar.getDayOfWeek() = this.get(Calendar.DAY_OF_WEEK)
