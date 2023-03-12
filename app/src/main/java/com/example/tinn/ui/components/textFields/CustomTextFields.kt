328b205245757678220886da2fa35aeb48a9a711
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-12 15:52:45 +0300

    Создание TextFieldWithButtonChange

--- app/src/main/java/com/example/tinn/ui/components/textFields/CustomTextFields.kt
+++ app/src/main/java/com/example/tinn/ui/components/textFields/CustomTextFields.kt
@@ -1,5 +1,6 @@
 package com.example.tinn.ui.components.textFields
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -8,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.example.tinn.ui.theme.Alpha
+import com.example.tinn.ui.theme.Blue
 
 @Composable
 fun TextFieldsWithLabelError(
@@ -50,3 +52,53 @@ fun TextFieldsWithLabelError(
     }
 }
 
+@Composable
+fun TextFieldsWithButtonChange(
+    value: String,
+    onValueChange: (newValue: String) -> Unit = {},
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    onSave: () -> Unit,
+    errorText: String = "",
+    labelText: String = "",
+    isError: Boolean = false,
+    modifier: Modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 32.dp),
+    keyboardOptions: KeyboardOptions = KeyboardOptions()
+) {
+    var isEnabled by remember { mutableStateOf(false) }
+
+    Column {
+        TextField(
+            value = value,
+            onValueChange = { text -> onValueChange(text) },
+            modifier = modifier,
+            enabled = isEnabled,
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Alpha
+            ),
+            trailingIcon = {
+                Text(
+                    text = if (isEnabled) "cохранить" else "изменить",
+                    color = Blue,
+                    modifier = Modifier.clickable {
+                        if (isEnabled) onSave()
+                        isEnabled = isEnabled.not()
+                    }
+                )
+            },
+            label = { Text(labelText) },
+            keyboardOptions = keyboardOptions,
+            visualTransformation = visualTransformation,
+            isError = isError
+        )
+
+        if (isError) {
+            Text(
+                text = errorText,
+                color = MaterialTheme.colors.error,
+                modifier = Modifier.padding(start = 40.dp)
+            )
+        }
+    }
+}
