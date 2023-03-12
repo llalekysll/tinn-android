6f8e70d8a70943eb67452149b60cd3e9a980c11f
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-12 11:53:57 +0300

    Изменение CalendarMain.kt

--- app/src/main/java/com/example/tinn/ui/components/calendarView/CalendarMain.kt
+++ app/src/main/java/com/example/tinn/ui/components/calendarView/CalendarMain.kt
@@ -2,18 +2,13 @@ package com.example.tinn.ui.components.calendarView
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import com.example.tinn.ui.components.calendarView.calendarContent.ChooseDayContent
+import com.example.tinn.ui.components.calendarView.calendarContent.ChooseYearContent
 import com.example.tinn.ui.components.calendarView.calendarHeader.CalendarHeader
-import com.example.tinn.utils.getDate
-import com.example.tinn.utils.getDayOfWeek
-import com.example.tinn.utils.getMonth
+import com.example.tinn.utils.getYear
 import java.util.*
 
 
@@ -25,8 +20,6 @@ fun CalendarView(
 ) {
     var currentDate = remember { mutableStateOf(Calendar.getInstance()) }
     var currentMode by remember { mutableStateOf("month") }
-    var days by remember { mutableStateOf(mutableListOf<Calendar>()) }
-    val weeks = listOf("Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс")
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -41,46 +34,23 @@ fun CalendarView(
             theme = calendarTheme.calendarHeaderTheme
         )
 
-        days = getDays(currentDate.value)
-        LazyVerticalGrid(columns = GridCells.Fixed(7), content = {
-            items(weeks) { weekDay ->
-                Text(
-                    weekDay,
-                    color = calendarTheme.calendarHeaderTheme.headerTextColor,
-                    textAlign = TextAlign.Center
-                )
+        if ("month" == currentMode) {
+            ChooseDayContent(
+                selectedDate = selectedDate,
+                currentDate = currentDate.value,
+                changeSelectedDate = { changeSelectedDate(it) },
+                calendarTheme = calendarTheme
+            )
+        } else {
+            ChooseYearContent(
+                currentDate.value.getYear(),
+                theme = calendarTheme.calendarItemYearTheme
+            ) {
+                val tempDate = currentDate.value.clone() as Calendar
+                tempDate.set(Calendar.YEAR, it)
+                currentDate.value = tempDate
             }
-            items(days) { day ->
-                val date = selectedDate?.getDate() ?: 0
-                val month = selectedDate?.getMonth() ?: 0
-                val isSelected = date == day.getDate() && month == day.getMonth()
+        }
 
-                CalendarItem(
-                    day = day.getDate().toString(),
-                    isSelected = isSelected,
-                    calendarItemTheme = calendarTheme.calendarItemTheme
-                ) {
-                    changeSelectedDate(day)
-                }
-            }
-        })
     }
-}
-private fun getDays(currentDate: Calendar): MutableList<Calendar> {
-
-    val tempDate = currentDate.clone() as Calendar
-    tempDate.set(Calendar.DATE, 1)
-
-    var countDayBeforeCurrentMonth = tempDate.getDayOfWeek() - 1
-    if (countDayBeforeCurrentMonth == 0) countDayBeforeCurrentMonth = 7
-    tempDate.add(Calendar.DATE, 1 - countDayBeforeCurrentMonth)
-
-    val tempDays = mutableListOf<Calendar>()
-    val countDayInMonth =
-        currentDate.getActualMaximum(Calendar.DAY_OF_MONTH) + countDayBeforeCurrentMonth - 1
-    repeat(countDayInMonth) {
-        tempDays.add(tempDate.clone() as Calendar)
-        tempDate.add(Calendar.DATE, 1)
-    }
-    return tempDays
 }
