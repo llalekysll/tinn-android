32f519f1510fa66ddcab8958828ab74fe7e48d05
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-12 16:49:06 +0300

    Проучение в настройках информации о пользователе

--- app/src/main/java/com/example/tinn/ui/features/main/settings/MainSettingsScreen.kt
+++ app/src/main/java/com/example/tinn/ui/features/main/settings/MainSettingsScreen.kt
@@ -7,32 +7,51 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tinn.ui.components.HorizontalSpacer
 import com.example.tinn.ui.theme.Blue
+import com.example.tinn.viewModel.UserViewModel
 
 @Composable
 fun MainSettingsScreen() {
-    var selectedPage by remember { mutableStateOf(PagesSettings.COMMON)}
+    val viewModel: UserViewModel = viewModel()
+    LaunchedEffect(key1 = Unit, block = {
+        viewModel.getUserInfo()
+    })
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Card(
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-            elevation = 8.dp
+    var selectedPage by remember { mutableStateOf(PagesSettings.COMMON) }
+    val user by viewModel.userInfo.observeAsState()
+
+    user?.let {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(Modifier.fillMaxWidth().padding(8.dp)) {
-                LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                    items(pagesSettingsList) {
-                        ItemCategoryPages(it, selectedPage == it) { selectedPage = it }
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                elevation = 8.dp
+            ) {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                        items(pagesSettingsList) {
+                            ItemCategoryPages(it, selectedPage == it) { selectedPage = it }
+                        }
                     }
+
+                    CommonSettingsView(viewModel, it)
                 }
             }
         }
