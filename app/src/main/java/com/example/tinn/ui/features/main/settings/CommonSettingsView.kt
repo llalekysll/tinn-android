612628db3770007a04af30ca221f642514634e59
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-13 18:25:38 +0300

    Реализация фронта блоков тип аккаунта и настройки контента

--- app/src/main/java/com/example/tinn/ui/features/main/settings/CommonSettingsView.kt
+++ app/src/main/java/com/example/tinn/ui/features/main/settings/CommonSettingsView.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tinn.data.modelForJSON.ResponceDataUserModel
+import com.example.tinn.ui.components.CheckBoxWithLabel
 import com.example.tinn.ui.components.TextWithCaption
 import com.example.tinn.ui.components.textFields.TextFieldEmailWithButtonChange
 import com.example.tinn.ui.components.textFields.TextFieldPhoneNumberWithButtonChange
@@ -38,14 +39,42 @@ fun CommonSettingsView(
         var phone by remember { mutableStateOf(user.userProfiles.phone ?: "") }
         var url by remember { mutableStateOf(user.userProfiles.url ?: "") }
         var language by remember { mutableStateOf("") }
+        var isCreatedContent by remember { mutableStateOf(true) }
+        var isShowRecommended by remember { mutableStateOf(true) }
+        var isShowPopular by remember { mutableStateOf(true) }
 
         TextWithCaption(caption = "Тема") {
             TextButton(onClick = { changeTheme() }) {
                 val isLight = MaterialTheme.colors.isLight
                 Text(
                     text = if (isLight) "Светлая" else "Темная",
+                    modifier = Modifier.padding(start = 4.dp),
                     color = MaterialTheme.colors.primary,
                     textDecoration = TextDecoration.Underline
+                )
+            }
+        }
+
+        TextWithCaption(caption = "Тип аккаунта") {
+            CheckBoxWithLabel(
+                label = "Создатель контента",
+                checked = isCreatedContent,
+                onCheckedChanged = { isCreatedContent = isCreatedContent.not() }
+            )
+        }
+
+        TextWithCaption(caption = "Настройки контента") {
+            Column {
+                CheckBoxWithLabel(
+                    label = "Показывать блок \"Рекомендуемые\"",
+                    checked = isShowRecommended,
+                    onCheckedChanged = { isShowRecommended = isShowRecommended.not() }
+                )
+
+                CheckBoxWithLabel(
+                    label = "Показывать блок \"Популярное\"",
+                    checked = isShowPopular,
+                    onCheckedChanged = { isShowPopular = isShowPopular.not() }
                 )
             }
         }
@@ -55,6 +84,7 @@ fun CommonSettingsView(
             onValueChange = { email = it },
             onSave = { }
         )
+
 
         TextFieldPhoneNumberWithButtonChange(
             value = phone,
@@ -66,7 +96,7 @@ fun CommonSettingsView(
             value = url,
             onSave = { viewModel.putUserInfo(hashMapOf(Pair("url", url))) },
             onValueChange = { url = it },
-            labelText = "Адрес профиля"
+            caption = "Адрес профиля"
         )
     }
 }
