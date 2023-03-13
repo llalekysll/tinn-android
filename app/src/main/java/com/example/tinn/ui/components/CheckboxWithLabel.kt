e849367e9a882a543268ecabf6620a9cc84009d0
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-13 18:22:22 +0300

    Изменение внешнего вида CheckboxWithLabel.kt

--- app/src/main/java/com/example/tinn/ui/components/CheckboxWithLabel.kt
+++ app/src/main/java/com/example/tinn/ui/components/CheckboxWithLabel.kt
@@ -4,23 +4,31 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Checkbox
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun CheckBoxWithLabel(checked: Boolean, onCheckedChanged: (newValue: Boolean) -> Unit) {
+fun CheckBoxWithLabel(
+    label: String,
+    checked: Boolean,
+    onCheckedChanged: (newValue: Boolean) -> Unit
+) {
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(end = 32.dp),
-        horizontalArrangement = Arrangement.End,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = "Запомнить")
-        Checkbox(checked = checked, onCheckedChange = { onCheckedChanged(it) })
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { onCheckedChanged(it) },
+            colors = CheckboxDefaults.colors(
+                checkedColor = MaterialTheme.colors.primary
+            )
+        )
+        Text(text = label)
     }
 }
